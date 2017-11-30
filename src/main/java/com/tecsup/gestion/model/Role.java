11abc69355f27f52c7be37bc6ba640ece9f03b28
Honda <eduardo@gmail.com>
2017-11-30 12:54:43 -0500

    spring

---
+++ src/main/java/com/tecsup/gestion/model/Role.java
@@ -0,0 +1,37 @@
+package com.tecsup.gestion.model;
+
+public class Role {
+	
+	String roleId;
+	String description;
+	
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Role(String roleId, String description) {
+		super();
+		this.roleId = roleId;
+		this.description = description;
+	}
+
+	public String getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Role [roleId=" + roleId + ", description=" + description + "]";
+	}
+
+}
---
+++ src/main/java/com/tecsup/gestion/services/EmployeeServiceImpl.java
@@ -0,0 +1,81 @@
+package com.tecsup.gestion.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tecsup.gestion.dao.EmployeeDAO;
+import com.tecsup.gestion.exception.DAOException;
+import com.tecsup.gestion.exception.EmptyResultException;
+import com.tecsup.gestion.model.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDAO employeeDAO;
+
+	@Override
+	public Employee find(int employee_id) throws DAOException, EmptyResultException {
+		
+		Employee emp = employeeDAO.findEmployee(employee_id);
+
+		return emp;
+	}
+
+	@Override
+	public List<Employee> findAll()
+			throws DAOException, EmptyResultException {
+		
+		List<Employee> emps = employeeDAO.findAllEmployees();
+	
+		return emps;
+	}
+
+	@Override
+	public void update(String login, String password, String lastname, String firstname, int salary, int dptId)
+			throws DAOException {
+	
+		employeeDAO.update(login, password, lastname, firstname, salary, dptId);
+	}
+
+	@Override
+	public void delete(String login) throws DAOException {
+		 
+		employeeDAO.delete(login);
+	
+	}
+
+	@Override
+	public void create(String login, String password, String lastname, String firstname, int salary, int dptId)
+			throws DAOException {
+	
+		employeeDAO.create(login, password, lastname, firstname, salary, dptId);
+
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { DAOException.class })
+	public void createWithRole(String login, String password, String lastname, String firstname, int salary, int dptId,
+			String roleId) throws DAOException {
+
+		employeeDAO.create(login, password, lastname, firstname, salary, dptId);
+		employeeDAO.addRole(login, roleId);
+
+	}
+
+	/*@Override
+	public List<Employee> findAllRoles() throws DAOException, EmptyResultException {
+		
+		List<Employee> emps = employeeDAO.findAllEmployees();
+		
+		return emps;
+	}*/
+
+	
+	
+
+}
